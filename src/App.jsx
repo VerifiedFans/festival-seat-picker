1ab71e94a18ece293839a7b83850bc1136564c80
VerifiedFans <troy@knoxconcepts.com>
2025-05-13 12:19:56 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
-import SeatPicker from "./SeatPicker";
+import FestivalSeatPicker from "./SeatPicker";
 import ContactForm from "./ContactForm";
 
 export default function App() {
   const [selectedSeats, setSelectedSeats] = useState([]);
 
   return (
-    <div className="App">
-      <h1>🎟 Singing in the Smokies Ticket Selector</h1>
-      <SeatPicker onSeatSelect={setSelectedSeats} />
+    <div>
+      <FestivalSeatPicker onSelect={setSelectedSeats} />
       <ContactForm selectedSeats={selectedSeats} />
     </div>
   );
